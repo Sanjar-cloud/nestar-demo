3a54d95258035494fd02155555a0ffd0fc24bb13
sanjar <sanjarbekkarimov229@gmail.com>
2026-09-14 03:35:47 +0900

    fix: homepage and layouts

--- pages/community/index.tsx
+++ pages/community/index.tsx
@@ -1,14 +1,13 @@
 import withLayoutBasic from "@/libs/components/layout/LayoutBasic";
-import { NextPage } from "next";
 import { useState } from "react";
 
-const Community: NextPage = () => {
-  console.log("COMMUNITY COMPONENT - PAGES ROUTER");
+const Community = () => {
   const [title, setTitle] = useState<string>("hello");
+
   return (
     <div>
-      COMMUNITY{" "}
-      <button onClick={() => alert("Hello MIT")} style={{ margin: "15px" }}>
+      COMMUNITY {""}
+      <button onClick={() => alert("Hello MT!")} style={{ margin: "15px" }}>
         PressMe
       </button>
     </div>
